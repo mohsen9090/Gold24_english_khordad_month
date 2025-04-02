33ebc77173190d4e539e2000f55abd5420576aec
mohsen9090 <mohsenbanihashemi4@gmail.com>
2025-04-02 11:26:09 +0000

    اضافه کردن فایل‌های جدید - اسلنگ، مکالمه و خرید

--- English/test.php
+++
@@ -1,9 +0,0 @@
-<?php $transaction_hash = 
-"f18b2544dc12953842b8b147ae80ccd84b66991ef61c4e89456a835bc78f7b21"; 
-if (preg_match('/^[a-fA-F0-9]{64}$/', 
-$transaction_hash)) {
-    echo "فرمت هش تراکنش صحیح است.";
-} else {
-    echo "فرمت هش تراکنش نامعتبر است.";
-}
-?>
